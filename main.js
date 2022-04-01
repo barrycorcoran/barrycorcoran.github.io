c416ab83db7fe6788d6449dfd2f1e8a451edf570
Barry Corcoran <102159093+barrycorcoran@users.noreply.github.com>
2022-04-01 10:17:04 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,87 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve);
+  });
+};
+
+(function () {
+  const prepared = document.createElement("template");
+  prepared.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `;
+  class SamplePiePrepared extends HTMLElement {
+    constructor() {
+      super();
+
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+
+      this._root = this._shadowRoot.getElementById("root");
+
+      this._props = {};
+
+      this.render();
+    }
+
+    onCustomWidgetResize(width, height) {
+      this.render();
+    }
+
+    async render() {
+      await getScriptPromisify(
+        "https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js"
+      );
+
+      const chart = echarts.init(this._root);
+      const option = {
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          top: "5%",
+          left: "center",
+        },
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            radius: ["40%", "70%"],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: "#fff",
+              borderWidth: 2,
+            },
+            label: {
+              show: false,
+              position: "center",
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: "40",
+                fontWeight: "bold",
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              { value: 1048, name: "Search Engine" },
+              { value: 735, name: "Direct" },
+              { value: 580, name: "Email" },
+              { value: 484, name: "Union Ads" },
+              { value: 300, name: "Video Ads" },
+            ],
+          },
+        ],
+      };
+      chart.setOption(option);
+    }
+  }
+
+  customElements.define("github-sap-demo-sample-chart", SamplePiePrepared);
+})();
