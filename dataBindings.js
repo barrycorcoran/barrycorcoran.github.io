a350d4172a9ff39797f12ca8c1d68bc7fb2783d0
CustomWidget <101722744+CustomWidget@users.noreply.github.com>
2022-04-14 15:25:35 +0200

    Update dataBindings.js

--- dataBindings.js
+++ dataBindings.js
@@ -1,98 +1,101 @@
 var getScriptPromisify = (src) => {
   return new Promise((resolve) => {
-    $.getScript(src, resolve);
-  });
-};
+    $.getScript(src, resolve)
+  })
+}
 
 (function () {
-  const prepared = document.createElement("template");
+  const prepared = document.createElement("template")
   prepared.innerHTML = `
         <style>
         </style>
         <div id="root" style="width: 100%; height: 100%;">
         </div>
-      `;
+      `
   class SamplePiePrepared extends HTMLElement {
     constructor() {
-      super();
+      super()
 
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+      this._shadowRoot = this.attachShadow({ mode: "open" })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
 
-      this._root = this._shadowRoot.getElementById("root");
+      this._root = this._shadowRoot.getElementById("root")
 
-      this._props = {};
+      this._props = {}
 
-      this.render();
+      this.render()
     }
 
     onCustomWidgetResize(width, height) {
-      this.render();
+      this.render()
     }
+
     set myDataSources(dataBindings) {
-      this._MyDataSources = dataBindings;
-      this.render();
+      this._myDataSource = dataBinding
+      this.render()
     }
 
     async render() {
       await getScriptPromisify(
         "https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js"
-      );
+      )
 
-      if (!this._myDataSources || this._MyDataSources.state !== "success") {
-        return;
+   
+
+      if (!this._myDataSource || this._myDataSource.state !== 'success') {
+        return
       }
-
-      const dimension = this._MyDataSources.metadata.feeds.dimensions.values[0];
-      const measure = this._MyDataSources.metadata.feeds.measures.values[0];
-      const data = this._MyDataSources.data.map((data) => {
+   
+      const dimension = this._myDataSource.metadata.feeds.dimensions.values[0]
+      const measure = this._myDataSource.metadata.feeds.measures.values[0]
+      const data = this._myDataSource.data.map(data => {
         return {
           name: data[dimension].label,
-          value: data[measure].raw,
-        };
-      });
+          value: data[measure].raw
+        }
+      })
 
-      const chart = echarts.init(this._root);
+      const myChart = echarts.init(this._root, 'wight')
       const option = {
         tooltip: {
-          trigger: "item",
+          trigger: 'item'
         },
         legend: {
-          top: "5%",
-          left: "center",
+          top: '2%',
+          left: 'center'
         },
         series: [
           {
-            name: "Access From",
-            type: "pie",
-            radius: ["40%", "70%"],
+            name: '',
+            type: 'pie',
+            radius: ['40%', '65%'],
             avoidLabelOverlap: false,
             itemStyle: {
               borderRadius: 10,
-              borderColor: "#fff",
-              borderWidth: 2,
+              borderColor: '#fff',
+              borderWidth: 4
             },
             label: {
               show: false,
-              position: "center",
+              position: 'center'
             },
             emphasis: {
               label: {
                 show: true,
-                fontSize: "40",
-                fontWeight: "bold",
-              },
+                fontSize: '25',
+                fontWeight: 'bold'
+              }
             },
             labelLine: {
-              show: false,
+              show: false
             },
-            data,
-          },
-        ],
-      };
-      chart.setOption(option);
+            data
+          }
+        ]
+      }
+      myChart.setOption(option)
     }
   }
 
-  customElements.define("github-sap-demo-sample-chart", SamplePiePrepared);
-})();
+  customElements.define("github-sap-demo-sample-chart", SamplePiePrepared)
+})()
