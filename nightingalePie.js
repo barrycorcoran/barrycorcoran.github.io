b83b9bec741e3480c3ce3b4e420572ecdfc29f89
Barry Corcoran <102159093+barrycorcoran@users.noreply.github.com>
2022-05-04 09:19:38 +0100

    Create nightingalePie.js

---
+++ nightingalePie.js
@@ -0,0 +1,89 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve);
+  });
+};
+
+(function () {
+  const prepared = document.createElement("template");
+  prepared.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `;
+  class NightingalePiePrepped extends HTMLElement {
+    constructor() {
+      super();
+
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+
+      this._root = this._shadowRoot.getElementById("root");
+
+      this._props = {};
+
+      this.render();
+    }
+
+    onCustomWidgetResize(width, height) {
+      this.render();
+    }
+
+    set myDataSource(dataBinding) {
+      this._myDataSource = dataBinding;
+      this.render();
+    }
+
+    async render() {
+      await getScriptPromisify(
+        "https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js"
+      );
+
+      if (!this._myDataSource || this._myDataSource.state !== "success") {
+        return;
+      }
+
+      const dimension = this._myDataSource.metadata.feeds.dimensions.values[0];
+      const measure = this._myDataSource.metadata.feeds.measures.values[0];
+      const data = this._myDataSource.data.map((data) => {
+        return {
+          name: data[dimension].label,
+          value: data[measure].raw,
+        };
+      });
+
+      const myChart = echarts.init(this._root, "wight");
+      const option = {
+        legend: {
+          top: "bottom",
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true },
+          },
+        },
+        series: [
+          {
+            name: "Nightingale Chart",
+            type: "pie",
+            radius: [50, 250],
+            center: ["50%", "50%"],
+            roseType: "area",
+            itemStyle: {
+              borderRadius: 8,
+            },
+            data,
+          },
+        ],
+      };
+      myChart.setOption(option);
+    }
+  }
+
+  customElements.define("github-sap-nightingale-pie", NightingalePiePrepped);
+})();
